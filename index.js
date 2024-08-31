cc8de3d899892abb3aae90ca80e4801976a2515a
Moh1tsingh <thakurmohitsingh2003@gmail.com>
2024-08-31 19:31:55 +0530

    add return to dfs and bfs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Stack = require("./data-structures/Stack/stack");
 const Queue = require("./data-structures/Queue/queue");
+const LinkedList = require("./data-structures/Linked-List/LinkedList");
 const BubbleSort = require("./algorithms/BubbleSort");
 const SelectionSort = require("./algorithms/SelectionSort");
 const InsertionSort = require("./algorithms/InsertionSort");
@@ -7,7 +8,6 @@ const QuickSort = require("./algorithms/QuickSort");
 const MergeSort = require("./algorithms/MergeSort");
 const LinearSearch = require("./algorithms/LinearSearch");
 const BinarySearch = require("./algorithms/BinarySearch");
-const LinkedList = require("./data-structures/Linked-List/LinkedList");
 const BreadthFirstSearch = require("./algorithms/BreadthFirstSearch");
 const DepthFirstSearch = require("./algorithms/DepthFirstSearch");
 
